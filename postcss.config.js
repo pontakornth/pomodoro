f1becfd7bd3add2ecfd4fe9f1778d0654ad76ccb
pontakornth <pontakorn61@gmail.com>
2020-06-20 15:14:15 +0700

    styling: config tailwind and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    autoprefixer(),
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      display: '"Chakra Petch", sans-serif',
+      body: '"Chakra Petch", sans-serif',
+    },
     extend: {},
   },
   variants: {},
