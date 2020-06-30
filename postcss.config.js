6f6f252c354666bfa518eb38c173debed82820df
pontakornth <pontakorn61@gmail.com>
2020-06-30 13:21:07 +0700

    chore: Install PostCSS to increase performance

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
+/* eslint-disable global-require */
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer(),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    IN_PRODUCTION && require('@fullhuman/postcss-purgecss')({
+      content: ['./public/**/*.html', './src/**/*.vue'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      whitelist: [],
+      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+    }),
   ],
 };
